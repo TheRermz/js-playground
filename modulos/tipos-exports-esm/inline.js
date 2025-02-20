f0c12129c4910fdac765856bea33fadd91e899ac
Murilo Fischer <mufischer@hotmail.com>
2025-02-20 12:20:39 -0300

    tipos de export

---
+++ modulos/tipos-exports-esm/inline.js
@@ -0,0 +1,7 @@
+export const inline = () => {
+  console.log("export inline");
+};
+
+export default function defaultInline() {
+  console.log("export default inline");
+}
---
+++ modulos/tipos-exports-esm/non-inline.js
@@ -0,0 +1,10 @@
+const group = () => {
+  console.log("group");
+};
+
+const exportDefault = () => {
+  console.log("export default");
+};
+
+export { group };
+export default exportDefault;
