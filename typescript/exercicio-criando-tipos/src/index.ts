f8919faf05b1054d5a73148209f3c82c28d97742
Murilo Fischer <mufischer@hotmail.com>
2025-02-28 16:24:31 -0300

    Implement planet rendering and form submission for adding new planets in TypeScript

--- typescript/exercicio-criando-tipos/src/index.ts
+++ typescript/exercicio-criando-tipos/src/index.ts
@@ -81,6 +81,94 @@ const listPlanets = () => {
   });
 };
 
-/**
- * TODO --> FAZER A PARTE DE MOSTRAR NO INDEX.HTML
- */
+const root = document.querySelector(`.root`);
+
+const renderPlanets = (planet: {
+  name: string;
+  coord: [Number, Number, Number, Number];
+  situation: PlanetSituation;
+  satellites?: Array<string>;
+}) => {
+  const article = document.createElement(`article`);
+  article.classList.add(`planet-article`);
+
+  const h1 = document.createElement(`h1`);
+  h1.classList.add(`planet-name`);
+  h1.textContent = planet.name;
+
+  const pCoord = document.createElement(`p`);
+  pCoord.classList.add(`planet-coord`);
+  pCoord.textContent = planet.coord.toString();
+
+  const pSituation = document.createElement(`p`);
+  pSituation.classList.add(`planet-situation`);
+  pSituation.textContent = planet.situation;
+
+  const pSatellites = document.createElement(`p`);
+  pSatellites.classList.add(`planet-satellites`);
+  pSatellites.textContent = planet.satellites.join(", ");
+
+  article.append(h1, pCoord, pSituation, pSatellites);
+  root.appendChild(article);
+};
+
+addPlanet({
+  name: `Terra`,
+  coord: [2, 3, 6, 7],
+  situation: "Habitado",
+  satellites: [`Lua`],
+});
+addPlanet({
+  name: `Venus`,
+  coord: [2, 3, 6, 7],
+  situation: "Inabitável",
+  satellites: [],
+});
+
+document.addEventListener(`DOMContentLoaded`, () => {
+  planets.forEach(renderPlanets);
+});
+
+const form = document.querySelector(`form`);
+
+const processCoordinateInput = (input: string) => {
+  const numbers = input.split(",").map((num) => Number(num.trim()));
+
+  if (numbers.length !== 4 || numbers.some(isNaN)) {
+    console.warn("Entrada inválida! Digite 4 números separados por vírgula.");
+    return null;
+  }
+
+  return numbers;
+};
+
+form.addEventListener(`submit`, (e) => {
+  e.preventDefault();
+
+  const planetName = document.querySelector(`#planet-name`) as HTMLInputElement;
+  const planetCoord = document.querySelector(`#coord`) as HTMLInputElement;
+  const planetSituation = document.querySelector(
+    `#situation`
+  ) as HTMLInputElement;
+  const planetSatellites = document.querySelector(
+    `#satellites`
+  ) as HTMLInputElement;
+
+  const newPlanet = {
+    name: planetName.value,
+    coord: processCoordinateInput(planetCoord.value) as [
+      number,
+      number,
+      number,
+      number
+    ],
+    situation: planetSituation.value as PlanetSituation,
+    satellites: planetSatellites.value.split(",").map((s) => s.trim()),
+  };
+
+  planets.push(newPlanet);
+  planetName.value = "";
+  planetCoord.value = "";
+  planetSatellites.value = "";
+  renderPlanets(newPlanet);
+});
